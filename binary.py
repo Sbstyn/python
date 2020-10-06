52f5cc2bf31749de92362ad68ce6c654b8feff94
Sbstyn <54476814+Sbstyn@users.noreply.github.com>
2020-10-06 19:43:04 +0200

    Add files via upload

---
+++ binary.py
@@ -0,0 +1,22 @@
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+x = 0 
+
+if num1 == 1:
+    x = x + 4
+else:
+    x = x
+
+if num2 == 1:
+    x = x + 2
+else:
+    x = x
+
+if num3 == 1:
+    x = x + 1
+else:
+    x = x
+
+print(x)
---
+++ chat.py
@@ -0,0 +1,9 @@
+import random
+input()
+x = random.randint(1,3)
+if x == 1:
+    print("Nem")
+elif x == 2:
+    print("Igen")
+elif x == 3:
+    print("Talán")
