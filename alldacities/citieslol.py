8955e8c2d69411c0122f6bc34e30c9c32cb083ac
Sbstyn <54476814+Sbstyn@users.noreply.github.com>
2020-10-21 21:12:47 +0200

    Update citieslol.py

--- alldacities/citieslol.py
+++ alldacities/citieslol.py
@@ -1,29 +1,5 @@
 import pyautogui
 import time
-
-time.sleep(5)
-"""f = open("C:\\Users\\sebes\\Desktop\\web\\alldacities\\alban.txt", "r")
-for words in f:
-    pyautogui.typewrite(words)
-    pyautogui.press("enter")
-
-f = open("C:\\Users\\sebes\\Desktop\\web\\alldacities\\andorra.txt", "r")
-for words in f:
-    pyautogui.typewrite(words)
-    pyautogui.press("enter")
-
-f = open("C:\\Users\\sebes\\Desktop\\web\\alldacities\\austria.txt", "r")
-for words in f:
-    pyautogui.typewrite(words)
-    pyautogui.press("enter")"""
-
-f = open("C:\\Users\\sebes\\Desktop\\web\\alldacities\\top5h.txt", "r")
-for words in f:
-    pyautogui.typewrite(words)
-    pyautogui.press("enter")
-    
-import pyautogui
-import time
 import keyboard
 
 x = 0
@@ -31,7 +7,8 @@ x = 0
 print("opening file")
 time.sleep(5)
 
-f = open("C:\\Users\\tanulo\\Desktop\\alldacities\\68k.txt")
+f = open("C:\\Users\\Marton\\Desktop\\web\\pys\\cities.txt")
+count = len(open("C:\\Users\\Marton\\Desktop\\web\\pys\\cities.txt").readlines(  ))
 
 print("started")
 
@@ -39,16 +16,8 @@ for words in f:
     pyautogui.typewrite(words)
     pyautogui.press("enter")
     x += 1
-    if x == 1:
-        print("1/68000")
-    elif x == 10000:
-        print("10000/68000")
-    elif x == 10000:
-        print("30000/68000")
-    elif x == 10000:
-        print("50000/68000")
-    elif x == 10000:
-        print("60000/68000")
+    if x % 10000 == 0 or x == 1:
+        print(x, "/", count)
     if keyboard.is_pressed('q'):
+        print("## Keyboard interrupt ##")
         break
-        print("Keyboard interrupt")
