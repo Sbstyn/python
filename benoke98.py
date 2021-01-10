830f0f06cc2a9d073a601824bb704d9a074735a2
Sbstyn <54476814+Sbstyn@users.noreply.github.com>
2021-01-10 19:56:21 +0100

    Update benoke98.py

--- benoke98.py
+++ benoke98.py
@@ -10,21 +10,22 @@ x = 0
 y = 0
 z = 0
 
-while not keyboard.is_pressed("esc"):
-    while a <= 26:
-        while z <= 26:
-            while y <= 26:
-                while x <= 26:
-                    #print(string.ascii_letters[a], string.ascii_letters[z], string.ascii_letters[y], string.ascii_letters[x])
-                    pyautogui.doubleClick()
-                    k = string.ascii_letters[a], string.ascii_letters[z], string.ascii_letters[y], string.ascii_letters[x]
-                    pyautogui.write("".join(k))
-                    x += 1
-                y += 1
-                x = 0
-            z += 1
-            y = 0
-        a += 1
-        z = 0
-else:
-    quit()
+while a <= 26:
+    while z <= 26:
+        while y <= 26:
+            while x <= 26:
+                #print(string.ascii_letters[a], string.ascii_letters[z], string.ascii_letters[y], string.ascii_letters[x])
+                pyautogui.doubleClick()
+                k = string.ascii_letters[a], string.ascii_letters[z], string.ascii_letters[y], string.ascii_letters[x]
+                pyautogui.write("".join(k))
+                pyautogui.press("enter")
+                time.sleep(.5)
+                x += 1
+                if keyboard.is_pressed("esc"):
+                    quit()
+            y += 1
+            x = 0
+        z += 1
+        y = 0
+    a += 1
+    z = 0
