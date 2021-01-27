5e988e3126f8d8393811aa14e6ecf50026cc089f
Sbstyn <54476814+Sbstyn@users.noreply.github.com>
2021-01-27 16:19:53 +0100

    Delete sendemail.py

--- sendemail.py
+++
@@ -1,8 +0,0 @@
-import smtplib
-
-def sendGmail(mailAdress, mailSubject):
-    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    server.login("chesscapist@gmail.com", "sodeeP12")
-    server.sendmail("chesscapist@gmail.com", mailAdress, mailSubject)
-
-    server.quit()
